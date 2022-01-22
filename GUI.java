53ac0ed9b1195f8404c31ee51a254dbd86edff06
Swarit Choudhari <68472469+SwiftyProgrammer690@users.noreply.github.com>
2022-01-22 11:58:27 -0500

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -6,20 +6,29 @@ import java.awt.event.ActionListener;
 public class GUI implements ActionListener {
 
     private int count = 0;
+    private int boughtCount = 0;
+    private JLabel yeetLabel;
     private JLabel label;
     private JFrame frame;
     private JPanel panel;
+    private JButton buyPhone;
+
 
     public GUI() {
         frame = new JFrame();
         JButton button = new JButton("Click me!");
         button.addActionListener(this);
         label = new JLabel("Number of clicks: 0");
+        yeetLabel = new JLabel("Bought Items: 0");
+        JButton buyPhone = new JButton("Buy a Phone");
+        buyPhone.addActionListener(this);
         panel = new JPanel();
         panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
         panel.setLayout(new GridLayout(0, 1));
         panel.add(button);
         panel.add(label);
+        panel.add(yeetLabel);
+        panel.add(buyPhone);
 
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -36,5 +45,12 @@ public class GUI implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         count++;
         label.setText("Number of clicks: " + count);
+        if (count > 50) {
+            boughtCount++;
+            for (int i = 0; i < 50; i++) {
+                count--;
+            }
+            yeetLabel.setText("Bought Items: " + boughtCount);
+        }
     }
 }
